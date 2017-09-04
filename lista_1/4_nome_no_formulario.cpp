052649326ed9b3c67a39c0484790c99778473cf1
Leandro Alves <leandrosustenido@gmail.com>
2017-09-04 12:54:15 -0300

    nome no formulario

---
+++ lista_1/4_nome_no_formulario.cpp
@@ -0,0 +1,11 @@
+// 2_fibonacci_rapido
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string str;
+  getline(cin, str);
+  if(str.size() <= 80) cout << "YES" << endl;
+  else cout << "NO" << endl;
+  return 0;
+}
