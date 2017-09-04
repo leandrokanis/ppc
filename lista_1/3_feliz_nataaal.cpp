dedeefb09c6cf5992d3f778046bb2dcb5bbc1795
Leandro Alves <leandrosustenido@gmail.com>
2017-09-04 12:32:18 -0300

    feliz natal

---
+++ lista_1/3_feliz_nataaal.cpp
@@ -0,0 +1,11 @@
+// 3_feliz_nataaal
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int num=0;
+  cin >> num;
+  string amount_of_a(num, 'a');
+  cout << "Feliz nat" + amount_of_a + "l!" << endl;
+  return 0;
+}
